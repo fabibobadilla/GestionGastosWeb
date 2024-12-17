0902122743758dae6266027631c0ef90a9fe3be0
FBWinster22 <bobadillafabian02@gmail.com>
2024-12-17 16:52:26 -0300

    actualizacion

--- obj/Debug/net8.0/GestionGastosWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/GestionGastosWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionGastosWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eda15522b12daf2fff16170f2b0e70c8847e8bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03b809fa7cf1a1937c5e05437ba51d9c3e68dcb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionGastosWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionGastosWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
